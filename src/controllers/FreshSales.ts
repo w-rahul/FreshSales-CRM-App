bd8fcd4f8eda80d47acd22c99a009c146169f54d
John Doe <johndoe@email.com>
2024-09-04 22:54:43 +0530

    Added src

--- src/controllers/FreshSales.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-export const freshsalesRouter = express.Router()
-
-freshsalesRouter.post("/", (req,res) =>{
-    return res.json({
-        message : "Hello from FreshSales "
-    })
-})
